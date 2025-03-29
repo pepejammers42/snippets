477f3bdc833d2cf4ae797e2bc44560a90d0d6590
yu <sun_eric@outlook.com>
2025-03-29 12:56:29 -0400

    u

--- lua/luasnip-latex-snippets/luasnippets/tex/math-commands.lua
+++ lua/luasnip-latex-snippets/luasnippets/tex/math-commands.lua
@@ -510,37 +510,39 @@ vim.list_extend(M, single_command_math_snippets)
 local postfix_math_specs = {
 	mbb = {
 		context = { name = "mathbb", dscr = "math blackboard bold" },
-		command = { pre = "\\mathbb{", post = "}" }, -- Use standard escape
+		command = { pre = [[\mathbb{]], post = [[}]] },
 	},
 	mcal = {
 		context = { name = "mathcal", dscr = "math calligraphic" },
-		command = { pre = "\\mathcal{", post = "}" }, -- Use standard escape
+		command = { pre = [[\mathcal{]], post = [[}]] },
 	},
 	mscr = {
 		context = { name = "mathscr", dscr = "math script" },
-		command = { pre = "\\mathscr{", post = "}" }, -- Use standard escape
+		command = { pre = [[\mathscr{]], post = [[}]] },
 	},
 	mfr = {
 		context = { name = "mathfrak", dscr = "mathfrak" },
-		command = { pre = "\\mathfrak{", post = "}" }, -- Use standard escape
+		command = { pre = [[\mathfrak{]], post = [[}]] },
 	},
 	hat = {
 		context = { name = "hat", dscr = "hat" },
-		command = { pre = "\\hat{", post = "}" }, -- Use standard escape
+		command = { pre = [[\hat{]], post = [[}]] },
 	},
 	dot = {
 		context = { name = "dot", dscr = "dot" },
-		command = { pre = "\\dot{", post = "}" }, -- Use standard escape
+		command = { pre = [[\dot{]], post = [[}]] },
 	},
 	bar = {
 		context = { name = "bar", dscr = "bar (overline)" },
-		command = { pre = "\\overline{", post = "}" }, -- Use standard escape
+		command = { pre = [[\overline{]], post = [[}]] },
 	},
 	tld = {
 		context = { name = "tilde", priority = 500, dscr = "tilde" },
-		command = { pre = "\\tilde{", post = "}" }, -- Use standard escape
+		command = { pre = [[\tilde{]], post = [[}]] },
 	},
 }
+
+-- The rest of your first code block (loop creating snippets) remains the same
 
 local postfix_math_snippets = {}
 for k, v in pairs(postfix_math_specs) do
